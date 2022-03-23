f0111a0bae71789f7187798baf7bc4594dda3530
Mariano Guardia <mguardia50@gmail.com>
2022-03-23 17:28:53 -0300

    Sabores Mariano Guardia V0.6

--- enviar.php
+++ enviar.php
@@ -1,16 +1,16 @@
 <?php
-$nombre = $_POST ['nombre'];
-$email = $_POST ['email'];
-$telefono = $_POST ['telefono'];
-$asunto = $_POST ['asunto'];
-$consulta = $_POST ['consulta'];
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$telefono = $_POST['telefono'];
+$asunto = $_POST['asunto'];
+$consulta = $_POST['consulta'];
 
 $header = 'De: ' . $email . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type_ Text/plain";
 
-$mensaje = "Enviado por " . $nombre . " \r\n";
+$mensaje = "Enviado por "  . $nombre . " \r\n";
 $mensaje .= "Mail: " . $email . " \r\n";
 $mensaje .= "Telefono " . $telefono . " \r\n";
 $mensaje .= "Mensaje: " . $consulta . " \r\n";
@@ -19,4 +19,7 @@ $para = 'mguardia@sabores.com.ar';
 
 mail ($para, $asunto, utf8_decode($mensaje), $header);
 
+header ("Location:./html/mensaje.html");
+
 ?>
+
